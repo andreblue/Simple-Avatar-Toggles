61fd2aaf230af4732c895b3770ad82092e981801
andreblue <andreblue2u@live.com>
2023-04-08 21:51:26 -0400

    Init Commit

---
+++ Assets/Andreblue/Toggle Tools/Libs/InspectorFrame.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+using UnityEngine;
+using System;
+using AnimatorAsCode.V0;
+using VRC.SDK3.Avatars.Components;
+using AnimatorController = UnityEditor.Animations.AnimatorController;
+
+namespace Andreblue.ToggleTools
+{
+    public static class ToggleTools
+    {
+        //Using a ton of things from https://github.com/hai-vr/av3-animator-as-code/blob/main/Examples/AacExample.cs
+        public static void InsecptorWindow(Editor editor, SerializedObject s_Object, string anim_Prefix, Action createFunc, Action removeFunc = null)
+        {
+            var prop = s_Object.FindProperty(anim_Prefix);
+            if (prop.stringValue.Trim() == String.Empty) 
+            { 
+                prop.stringValue = GUID.Generate().ToString();
+                s_Object.ApplyModifiedProperties();
+            }
+
+            editor.DrawDefaultInspector();
+
+            if (GUILayout.Button("Create")) createFunc.Invoke();
+            if (removeFunc != null && GUILayout.Button("Remove")) removeFunc.Invoke();
+        }
+        public static AacFlBase AnimEditor(string systemName, VRCAvatarDescriptor avatar, AnimatorController assetContainer, string assetKey, bool writeDefaults = false)
+        {
+            var animCode = AacV0.Create(new AacConfiguration
+            {
+                SystemName = systemName,
+                AvatarDescriptor = avatar,
+                AnimatorRoot = avatar.transform,
+                DefaultValueRoot = avatar.transform,
+                AssetContainer = assetContainer,
+                AssetKey = assetKey,
+                DefaultsProvider = new AacDefaultsProvider(writeDefaults: writeDefaults)
+
+            });
+            animCode.ClearPreviousAssets();
+
+            return animCode;
+        }
+    }
+    
+    
+}
+#endif
---
+++ Assets/Andreblue/Toggle Tools/SimpleToggle.cs
@@ -0,0 +1,53 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VRC.SDK3.Avatars.Components;
+using UnityEditor.Animations;
+using UnityEditor;
+using AnimatorAsCode.V0;
+
+namespace Andreblue.ToggleTools
+{
+    public class SimpleToggle : MonoBehaviour
+    {
+        public VRCAvatarDescriptor avatar;
+        public AnimatorController assetContainer;
+        public string assetKey;
+        public bool writeDefaults;
+
+    }
+    [CustomEditor(typeof(SimpleToggle), true)]
+    public class SimpleToggle_Editor : Editor
+    {
+        private const string Name = "SP";
+
+        public override void OnInspectorGUI ()
+        {
+            ToggleTools.InsecptorWindow(this, serializedObject, "assetKey", Create, Remove);
+        }
+
+        private void Create ()
+        {
+            var selectedObject = (SimpleToggle)target;
+            var acc = ToggleTools.AnimEditor(Name, selectedObject.avatar, selectedObject.assetContainer, selectedObject.assetKey, selectedObject.writeDefaults);
+            var fx = acc.CreateSupportingFxLayer($"Toggle_{target.name}");//acc.CreateMainFxLayer();
+            var toggleOff = fx.NewState($"Toggle_{target.name}_Off")
+            .WithAnimation(acc.NewClip().Toggling(selectedObject.gameObject, false));
+            var toggleOn = fx.NewState($"Toggle_{target.name}_On")
+            .WithAnimation(acc.NewClip().Toggling(selectedObject.gameObject, true));
+            var toggleBool = fx.BoolParameter($"{Name}_Toggle_{target.name}");
+
+            toggleOff.TransitionsTo(toggleOn).When(toggleBool.IsTrue());
+            toggleOn.TransitionsTo(toggleOff).When(toggleBool.IsFalse());
+        }
+        private void Remove () 
+        {
+            var selectedObject = (SimpleToggle)target;
+            var acc = ToggleTools.AnimEditor(Name, selectedObject.avatar, selectedObject.assetContainer, selectedObject.assetKey, selectedObject.writeDefaults);
+            acc.RemoveAllSupportingLayers($"Toggle_{target.name}");
+        }
+    }
+}
+#endif
